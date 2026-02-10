177b6475189801a7cd5bc4ebcd5cd0b1fc9552db
sis061 <sis061@naver.com>
2026-02-10 21:32:22 +0900

    feat: 특식(special)추가 기능 개발. design: ui 변경

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ const slots = ["morning", "afternoon", "evening", "night"];
 
 type FeedLog = {
   date: string;
-  slot: "morning" | "afternoon" | "evening" | "night";
+  slot: "morning" | "afternoon" | "evening" | "night" | "special";
   done_at: string;
 };
 type CompletedMap = Partial<Record<FeedLog["slot"], string>>;
@@ -32,6 +32,9 @@ export default function Home() {
     minute: "2-digit",
     hour12: false,
   });
+
+  const showSpecial = !!completed.special;
+  const visibleSlots = showSpecial ? [...slots, "special"] : [...slots];
 
   function formatHM(iso: string) {
     return timeFmt.format(new Date(iso));
@@ -99,6 +102,9 @@ export default function Home() {
       case "night":
         slotKr = "저녁";
         break;
+      case "special":
+        slotKr = "특식";
+        break;
       default:
         slotKr = slot;
         break;
@@ -158,16 +164,22 @@ export default function Home() {
   return (
     <main className="wrapper">
       <section className="inner min-h-[calc(100dvh-10.75rem)] text-3xl !mx-auto !w-full h-full !mb-8 !p-2 flex flex-col items-center justify-start">
-        <div className="flex flex-col items-center !pb-4 !pt-4 justify-center max-w-100 w-full">
+        <div className="flex flex-col items-start !pb-4 !pt-4 justify-center max-w-100 w-full">
           <Today />
         </div>
-        <div className="border-2 bg-white rounded-md px-4 py-4 flex flex-col gap-4 shadow-md border-green-600 max-w-100 w-full">
+        <div
+          className={`border-2 bg-white rounded-md px-4 flex flex-col gap-4 shadow-md border-green-600 max-w-100 w-full relative ${!showSpecial ? "pb-12" : "pb-4"}`}
+        >
           <div className="flex gap-4 items-center justify-between border-b border-black/25 p-4 relative">
             <div className="flex flex-col gap-4 min-w-1/2 grow">
               <b className="text-4xl !text-green-400">유월이</b>
-              <ul className="flex flex-col *:text-lg *:!text-[#999]">
-                <li>하루에 4번</li>
-                <li>30g 씩 먹어요~</li>
+              <ul className="flex flex-col *:text-lg *:!text-[#999] gap-0 -ml-1">
+                <li>
+                  <span className="text-base">🐾</span> 하루에 4번
+                </li>
+                <li>
+                  <span className="text-base">🐾</span> 30g 씩 먹어요~
+                </li>
               </ul>
             </div>
             <div className="absolute -top-12 -right-4 z-30">
@@ -176,12 +188,13 @@ export default function Home() {
                 alt="카드 유월 이미지"
                 width={190}
                 height={190}
-                className="transition-transform duration-500 ease-in-out scale-100 cursor-pointer touch-manipulation active:scale-150 hover:scale-150"
+                loading="eager"
+                className="transition-transform duration-200 ease-in-out scale-100 cursor-pointer touch-manipulation active:scale-150"
               />
             </div>
           </div>
           <div className="p-4 w-full flex flex-col gap-8 items-center">
-            {slots.map((slot) => {
+            {visibleSlots.map((slot) => {
               const s = slot as FeedLog["slot"];
               const doneAt = completed[s];
               return (
@@ -192,7 +205,7 @@ export default function Home() {
                   <button
                     onClick={() => handleClick(s)}
                     disabled={!!doneAt}
-                    className={` transition shadow-2xs border border-[#99999925] rounded-lg !p-2 ${doneAt && "bg-green-500"} transition-transform duration-200 ease-in-out scale-100 cursor-pointer touch-manipulation active:scale-95 hover:scale-105`}
+                    className={` transition shadow-2xs border border-[#99999925] rounded-lg !p-2 ${doneAt && "bg-green-500"} transition-transform duration-200 ease-in-out scale-100 cursor-pointer touch-manipulation active:scale-95`}
                   >
                     <Check
                       strokeWidth={2.5}
@@ -216,20 +229,55 @@ export default function Home() {
               );
             })}
           </div>
+          {!showSpecial && (
+            <button
+              onClick={() => {
+                handleClick("special");
+              }}
+              className="border border-green-600 bg-green-500 w-20 h-20 absolute -bottom-10 left-1/2 -translate-x-1/2 rounded-full shadow-md flex flex-col items-center justify-center transition-transform duration-200 ease-in-out scale-100 cursor-pointer touch-manipulation active:scale-90"
+            >
+              <div className="relative w-full h-full">
+                <div className="w-20 h-20 absolute top-0 left-0">
+                  <svg viewBox="0 0 200 100" className="w-full h-full">
+                    <path
+                      id="curve"
+                      d="M 15 100 A 65 90 0 0 1 180 110"
+                      fill="transparent"
+                    />
+                    <text className="text-4xl font-bold fill-white">
+                      <textPath
+                        href="#curve"
+                        startOffset="50%"
+                        textAnchor="middle"
+                      >
+                        특 식 이 다 !
+                      </textPath>
+                    </text>
+                  </svg>
+                </div>
+              </div>
+
+              <span className="text-4xl mb-2">🍖</span>
+            </button>
+          )}
         </div>
 
         {undoSlot && (
           <div
             onClick={handleUndo}
-            className=" fixed left-1/2 -translate-x-1/2 bottom-6 shadow-lg min-w-64 transition-all duration-200 ease-in-out"
+            className="fixed left-1/2 -translate-x-1/2 bottom-6 shadow-lg max-w-[95%] transition-all duration-200 ease-in-out"
           >
-            <div className="bg-fill w-full h-full *:!text-white px-6 py-3  rounded-xl flex items-center justify-center gap-8 ">
-              <span className="text-base !text-white z-10">
-                {slotKr(undoSlot as FeedLog["slot"])} 밥 먹었어요!
+            <div className="bg-fill w-full h-full *:!text-white px-6 py-3 rounded-xl flex items-center justify-center gap-8 ">
+              <span className="text-base !text-white z-10 whitespace-nowrap overflow-hidden text-ellipsis">
+                <b className="text-base !text-white">
+                  <span>🦴 </span>
+                  {slotKr(undoSlot as FeedLog["slot"])}
+                </b>
+                을 먹었어요!
               </span>
               <button
                 onClick={handleUndo}
-                className=" flex items-center justify-center gap-1 font-semibold text-sm transition-transform duration-200 ease-in-out scale-100 cursor-pointer touch-manipulation active:scale-95 hover:scale-105"
+                className="whitespace-nowrap overflow-hidden text-ellipsis flex items-center justify-center gap-1 font-semibold text-sm transition-transform duration-200 ease-in-out scale-100 cursor-pointer touch-manipulation active:scale-95"
               >
                 <Undo2 size={12} color="#fff" className="!mb-1.5" />
                 되돌리기
